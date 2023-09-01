a48636eeab3e140f31ef5b5c81388a1e8ec52684
fefdfea <cankyu6@gmail.com>
2023-09-01 18:50:43 +0900

    화이트리스트 추가

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
+
+const whitelist = ["http://localhost:8080", "http://localhost:8081"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      // 만일 whitelist 배열에 origin인자가 있을 경우
+      callback(null, true); // cors 허용
+    } else {
+      callback(new Error("Not Allowed Origin!")); // cors 비허용
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use("/static", express.static(__dirname + "/static"));
 const uri = `${process.env.DB_URL}`;
